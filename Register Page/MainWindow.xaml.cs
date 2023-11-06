ededd7f8585a259c756754126f3f8abac8784469
XXX <XXX@DESKTOP-CQSHQ2K>
2023-11-06 16:32:59 +0100

    Add project files.

---
+++ Register Page/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace Register_Page
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+    }
+}
---
+++ Register Page/UserControls/My TextBox.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Register_Page.UserControls
+{
+   
+    public partial class My_TextBox : UserControl
+    {
+        public My_TextBox()
+        {
+            InitializeComponent();
+        }
+
+
+        public string Hint
+        {
+            get { return (string)GetValue(HintProperty); }
+            set { SetValue(HintProperty, value); }
+        }
+
+        public static readonly DependencyProperty HintProperty = DependencyProperty.Register
+            ("Hint", typeof(string), typeof(My_TextBox));
+    }
+}
